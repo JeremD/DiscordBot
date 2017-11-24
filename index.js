f85b67c8a2b497b0330c290890e2c60460b3da2b
Jyder <jyder@tuta.io>
2017-11-24 14:50:34 +0100

    Define Ike

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const client = new Discord.Client();
 // const token = process.env.BOT_TOKEN;
 const token = 'MzgyODc1NDgwNDUxMzE3NzYw.DPcEKg.wBEEnIzsJnHS_duaaSDOPTfEwpY';
 var prefix = "ike!";
-var mention = "<@382076625937891328>";
-var random;
+var id_bot = "<@382875480451317760>";
+var id_jyder = '361220724142112780';
 
+//Lancement bot
 client.on('ready', () => {
     console.log("Bonjour, je suis Ike, pour vous servir.");
-    client.user.setGame("Ike, ETHER !!!");
+    client.user.setGame("ETHER!!!");
 });
 
 client.on('message', message => {
-    var msgc = message.content;
+    var msgc = message.content; //raccourci
 
-    if (!message.author.bot ) {
-        if (message.author.id == '148727789640613888') { // Moi pour l'instant
+    if (!message.author.bot ) { //si l'auteur du message n'est pas un bot
             if (msgc.includes("ping")) {
                 message.channel.send('pong');
             } else {
                 message.channel.send(msgc);
             }
-        }
+
     }
 
 });
