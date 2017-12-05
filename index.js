d3042521f8ac92f4d5874c5d808747099f2eeaea
Jyder <jyder@tuta.io>
2017-12-05 15:43:09 +0100

    Update Ike

--- index.js
+++ index.js
@@ -34,28 +34,30 @@ client.on('message', message => { //*
           message.channel.send("J'adore ma SR : ETHER !!!");
         } else if (msgc.includes('ETHER') || msgc.includes('AETHER') || msgc.includes('Ether') || msgc.includes('ether')) {
           message.channel.send("Personne ne m'imite !");
-        } else if (msgc.includes("T'AS DIT QUOI LÀ") || msgc.includes("TA GUEULE") {
+        }
+      }
+      if (!message.author.bot) {
+        if (msgc.includes("T'AS DIT QUOI LÀ") || msgc.includes("TA GUEULE") {
             message.channel.send("OMAE WA MOU SHINDEIRU");
           }
-
-
-
-          //si mention après le spréfix
-          if (msgc.includes(prefix)) {
-            userp = msgc.substr(prefix.length);
-
-            if (userp == 'cc') {
-              message.channel.send('Coucou');
-            } else if (userp == 'jyder') {
-              message.channel.send('Moi aussi j\'aime les pâtes');
-            } else if (userp == 'rein') {
-              message.channel.send('Respect pour Reinhardt <:rein:368038551482400788> ');
-            } else if (userp == 'takumi') {
-              message.channel.send('Takumerde <:nohrianscum:377090236095791104> ');
-            }
-          }
-
         }
+
+        //si mention après le spréfix
+        if (msgc.includes(prefix)) {
+          userp = msgc.substr(prefix.length);
+
+          if (userp == 'cc') {
+            message.channel.send('Coucou');
+          } else if (userp == 'jyder') {
+            message.channel.send('Moi aussi j\'aime les pâtes');
+          } else if (userp == 'rein') {
+            message.channel.send('Respect pour Reinhardt <:rein:368038551482400788> ');
+          } else if (userp == 'takumi') {
+            message.channel.send('Takumerde <:nohrianscum:377090236095791104> ');
+          }
+        }
+
+
 
       }); //*
 
