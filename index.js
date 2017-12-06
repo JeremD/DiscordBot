396fa78af0016af4951a28e3782acf1d97263178
Runolas <jyder@tuta.io>
2017-12-06 10:36:23 +0100

    Update Ike

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ client.on('message', message => { //*
   //Si l'auteur est un bot
   if (message.author.bot) {
     if (msgc.includes("T'AS DIT QUOI LÀ") || msgc.includes("TA GUEULE")) {
-      message.channel.send("**OMAE WA MOU SHINDEIRU !!!**" + emoj_rein + emoj_rein + emoj_rein);
+      message.channel.send("**OMAE WA MOU SHINDEIRU " + emoj_rein + emoj_rein + emoj_rein + ' !!!**');
     }
   }
 
