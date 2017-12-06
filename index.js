1ddeaee95e8361bcf802449dbca68d2b50463783
Runolas <jyder@tuta.io>
2017-12-06 10:52:02 +0100

    Update Ike

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ client.on('message', message => { //*
 
   //Si l'auteur est un bot
   if (message.author.bot) {
-    if (msgc.includes("T'AS DIT QUOI LÀ")cipher || msgc.includes("TA GUEULE")) {
+    if (msgc.includes("T'AS DIT QUOI LÀ") || msgc.includes("TA GUEULE")) {
       message.channel.send("**OMAE WA MOU SHINDEIRU " + emoj_rein + emoj_rein + emoj_rein + ' !!!**');
     } else if (msgc == emoj_rein) {
       message.channel.send('Je voudrai une emote en mon nom s\'il vous plaît.')
