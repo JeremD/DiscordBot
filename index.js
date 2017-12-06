43b8031222f6333426f1e776abca33194642329f
Runolas <jyder@tuta.io>
2017-12-06 10:45:54 +0100

    Update Ike

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ client.on('message', message => { //*
     if (msgc.includes("T'AS DIT QUOI LÀ") || msgc.includes("TA GUEULE")) {
       message.channel.send("**OMAE WA MOU SHINDEIRU " + emoj_rein + emoj_rein + emoj_rein + ' !!!**');
     } else if (msgc.includes(emoj_rein)) {
-      message.channel.send('Je voudrais une emote en mon nom s\'il vous plaît.')
+      message.channel.send('Je voudrai une emote en mon nom s\'il vous plaît.')
     }
   }
 
@@ -74,7 +74,7 @@ client.on('message', message => { //*
       case 'fanart':
         message.channel.send("*NULL*");
         break;
-      case 'kick':
+      case 'kick @':
         message.channel.send("Désolé, je ne peux pas faire ça.");
         break;
       default:
