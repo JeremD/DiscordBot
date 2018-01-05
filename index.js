8e449e66f439939aeeda7268b7ec9b9edc9cb825
Runolas <jyder@tuta.io>
2018-01-05 11:31:20 +0100

    Update Ike

--- index.js
+++ index.js
@@ -38,10 +38,12 @@ client.on('message', message => { //*
     // }
     else if (msgc.includes('ik!kick') || msgc.includes('ik!ban')) {
       message.channel.send("Désolé, je ne peux pas faire ça.");
-    }
-    random = Math.floor((Math.random() * 6) + 1);
-    if (msgc.includes('Takumi' && random == 6)) {
-      message.channel.send("Hoshidian scum! " + emoj_tak);
+    } else if (msgc.includes('Takumi')) {
+      random = Math.floor((Math.random() * 6) + 1);
+      if (random == 6) {
+        message.channel.send("Hoshidian scum! " + emoj_tak);
+      }
+
     }
   }
 
